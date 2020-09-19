aefc84b213e10f1e21ea6df01d39ed4b0269739f
Darren Behan <darrenbehan@hotmail.com>
2020-09-19 19:04:14 +1000

    adding a sort button

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Form, FormControl, Button, DropdownButton, Dropdown } from 'react-bootstrap';
 import "./index.css";
 
 function Nav(props) {
@@ -9,6 +9,9 @@ function Nav(props) {
         <FormControl type="text" placeholder="Search Name" className="mr-sm-2" onChange={props.handleInputChange} value={props.value} />
         <Button onClick={props.handleFormSubmit} variant="outline-dark">Search</Button>
       </Form>
+      <DropdownButton id="dropdown-basic-button" variant="outline-dark" title="Sort By" drop="left">
+        <Dropdown.Item as="button" value="name" onClick={props.handleSortChange}>Name</Dropdown.Item>
+      </DropdownButton>
     </Navbar>
   );
 }
